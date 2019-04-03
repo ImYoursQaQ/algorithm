02c89a63e3c6f9fe5afddc1b49cb449151be7a66
Kimi John <1948976637@qq.com>
2019-04-03 23:18:45 +0800

    快速排序，使用递归

---
+++ src/sort/quickSort/main.java
@@ -0,0 +1,54 @@
+package sort.quickSort;
+
+/*
+ * 冒泡排序
+ *
+ * */
+public class main {
+    public static void main(String[] args) {
+        int[] a = {7, 8, 6, 4, 9, 5, 2, 3, 1};
+        int start = 0,end = a.length-1;
+        quickSort(a,start,end);
+        printresult(a);
+
+    }
+
+    static void quickSort(int[] a,int low,int high) {
+       if (low<high){//递归进入的条件
+           int k = parttion(a,low,high);
+           quickSort(a,low,k-1);
+           quickSort(a,k+1,high);
+       }
+
+
+    }
+
+    static void printresult(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    static int parttion(int[] a,int low,int high){
+        int temp = a[low];//将当前表中第一个元素作为枢轴元素
+        while (low<high){ //循环跳出条件，一趟排序完成
+            while (low<high&&a[high]>=temp){
+                high--;
+            }
+            a[low] = a[high];//从右端找出比枢轴元素小的
+            while (low<high&&a[low]<=temp){
+                low++;
+            }
+            a[high]= a[low];//从左端找出比枢轴元素大的
+
+        }
+        a[low] = temp;//将枢轴元素放在最终位置上
+        return low;
+    }
+}
