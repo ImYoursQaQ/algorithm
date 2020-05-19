007542f0262759280b1d91c9c13df73cd0fe9482
lalalahubo <zhanghailiang@daddychoice.net>
2020-05-19 22:52:57 +0800

    将工程升级为maven工程

--- src/main/java/StringUtils/addressSimaple/main.java
+++ src/main/java/StringUtils/addressSimaple/main.java
@@ -14,8 +14,8 @@ public class main {
     }
 
     private static String addressSimaple(String path) {
-        Deque<String> stack = new LinkedList<>();
-        Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
+        Deque<String> stack = new LinkedList();
+        Set<String> skip = new HashSet(Arrays.asList("..",".",""));
         for (String dir : path.split("/")) {
             if (dir.equals("..") && !stack.isEmpty()) stack.pop();
             else if (!skip.contains(dir)) stack.push(dir);
---
+++ src/main/java/benchmark/HelloJMH.java
@@ -0,0 +1,45 @@
+package benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class HelloJMH {
+    @Benchmark
+    public void wellHelloThere() {
+        // this method was intentionally left blank.
+    }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * You are expected to see the run with large number of iterations, and
+     * very large throughput numbers. You can see that as the estimate of the
+     * harness overheads per method call. In most of our measurements, it is
+     * down to several cycles per call.
+     *
+     * a) Via command-line:
+     *    $ mvn clean install
+     *    $ java -jar target/benchmarks.jar JMHSample_01
+     *
+     * JMH generates self-contained JARs, bundling JMH together with it.
+     * The runtime options for the JMH are available with "-h":
+     *    $ java -jar target/benchmarks.jar -h
+     *
+     * b) Via the Java API:
+     *    (see the JMH homepage for possible caveats when running from IDE:
+     *      http://openjdk.java.net/projects/code-tools/jmh/)
+     */
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(HelloJMH.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
