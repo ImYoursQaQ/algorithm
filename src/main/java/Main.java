2872fa83bd6a8191efe5b79458f840b0c295292a
huboit <hubo@daddychoice.net>
2020-06-10 10:15:04 +0800

    jvm 参数

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * JVM 参数和使用
+ * 1。-XX:+PrintGCDetails：打印gc详细信息
+ * 2。-Xloggc:log/gc.log：将gc日志保存到文件中
+ *
+ */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+        List list = new ArrayList();
+        for(int i=0;i<10000;i++){
+            String str = new String("12348888");
+            list.add(str);
+        }
+        System.out.println("最大堆："+Runtime.getRuntime().maxMemory()/1024/1024+"M");
+        System.out.println("空闲堆："+Runtime.getRuntime().freeMemory()/1024/1024+"M");
+        System.out.println("总的堆："+Runtime.getRuntime().totalMemory()/1024/1024+"M");
+        while (true){
+            System.in.read();
+        }
     }
 }
