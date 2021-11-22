3e601261514c2d0ba036ce4f32df378d8fefccf5
teohubo <teohubo@didiglobal.com>
2021-11-22 23:07:06 +0800

    feat: 原型模式

---
+++ src/main/java/designpattern/prototypepattern/Score.java
@@ -0,0 +1,16 @@
+package designpattern.prototypepattern;
+
+import lombok.Getter;
+
+/**
+ * @author teohubo
+ */
+@Getter
+public class Score {
+
+    private int number;
+
+    public Score(int number) {
+        this.number = number;
+    }
+}
