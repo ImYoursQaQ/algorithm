1a916f4398bf75f72529a0ae38512df208fbcee6
teohubo <teohubo@didiglobal.com>
2021-12-07 21:00:49 +0800

    feat:模版模式修改

--- src/main/java/designpattern/behavioral/templatepattern/pattern2/TemplateApp.java
+++ src/main/java/designpattern/behavioral/templatepattern/pattern2/TemplateApp.java
@@ -1,5 +1,9 @@
 package designpattern.behavioral.templatepattern.pattern2;
 
+import designpattern.behavioral.templatepattern.Order;
+
+import java.util.List;
+
 /**
  * @author teohubo
  */
@@ -8,6 +12,15 @@ public class TemplateApp {
     public static void main(String[] args) {
 
     }
+}
+
+class MyOrderHandler{
+
+    public void handlerOrders(List<Order> orders){
+
+
+    }
+
 }
 
 
