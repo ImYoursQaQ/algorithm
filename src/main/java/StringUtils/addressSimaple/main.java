b964fe5714a58e39099de330d0019d6d48582c1e
teohubo <teohubo@didiglobal.com>
2022-03-09 17:43:58 +0800

    style : 格式

--- src/main/java/StringUtils/addressSimaple/main.java
+++ src/main/java/StringUtils/addressSimaple/main.java
@@ -1,7 +1,7 @@
 package StringUtils.addressSimaple;
 
 import java.util.*;
-/*
+/**
 * 利用栈实现的路径化简算法
 *
 * */
@@ -9,19 +9,24 @@ import java.util.*;
 public class main {
     public static void main(String[] args) {
         String a = "/a/./b/.././c/";
-        String b = addressSimaple(a); //理论：/a/c
+        String b = addressSample(a); //理论：/a/c
         System.out.println(b);
     }
 
-    private static String addressSimaple(String path) {
-        Deque<String> stack = new LinkedList();
-        Set<String> skip = new HashSet(Arrays.asList("..",".",""));
+    private static String addressSample(String path) {
+        Deque<String> stack = new LinkedList<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
         for (String dir : path.split("/")) {
-            if (dir.equals("..") && !stack.isEmpty()) stack.pop();
-            else if (!skip.contains(dir)) stack.push(dir);
+            if ("..".equals(dir) && !stack.isEmpty()) {
+                stack.pop();
+            } else if (!skip.contains(dir)) {
+                stack.push(dir);
+            }
         }
         String res = "";
-        for (String dir : stack) res = "/" + dir + res;
+        for (String dir : stack) {
+            res = "/" + dir + res;
+        }
         return res.isEmpty() ? "/" : res;
     }
 }
