c66da6125f49b2563373bffc1df52901f0ff6e56
teohubo <teohubo@didiglobal.com>
2022-03-18 15:26:00 +0800

    feat:二分查找的应用。mark

--- src/main/java/leetCode/search/Solution704.java
+++ src/main/java/leetCode/search/Solution704.java
@@ -5,6 +5,13 @@ package leetCode.search;
  */
 class Solution704 {
 
+    /**
+     * 普通的二分查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search(int[] nums, int target) {
         if (nums.length == 0) {
             return -1;
@@ -23,5 +30,40 @@ class Solution704 {
             }
         }
         return -1;
+    }
+
+    /**
+     * 在有重复元素的排序数组中找第一个符合要求的元素
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchFirst(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution704 solution704 = new Solution704();
+        int[] nums = {1, 1, 3, 4, 5, 5, 6, 8, 10};
+        System.out.println(solution704.searchFirst(nums, 1));
     }
 }
