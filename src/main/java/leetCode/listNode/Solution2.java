644a13a3a255c00124d4c4a18fe65ed1b536e434
teohubo <teohubo@didiglobal.com>
2022-03-25 15:27:03 +0800

    feat：2链表求和

--- src/main/java/leetCode/listNode/Solution2.java
+++ src/main/java/leetCode/listNode/Solution2.java
@@ -23,14 +23,17 @@ public class Solution2 {
      * @return
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
         ListNode pre = new ListNode(0);
         ListNode cur = pre;
         int carry = 0;
         while (l1 != null || l2 != null) {
-            int a = l1 != null ? l1.val : 0;
-            int b = l2 != null ? l2.val : 0;
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
             int sum = a + b + carry;
-            carry = sum > 9 ? 1 : 0;
+            carry = sum / 10;
             cur.next = new ListNode(sum % 10);
             cur = cur.next;
             if (l1 != null) {
@@ -40,7 +43,7 @@ public class Solution2 {
                 l2 = l2.next;
             }
         }
-        if (carry == 1) {
+        if (carry == 1){
             cur.next = new ListNode(1);
         }
         return pre.next;
